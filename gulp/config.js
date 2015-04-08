8676b699e58f16f5d9dca63629157b1ddbb38b61
akabeko <akabekobeko@gmail.com>
2015-04-08 22:03:16 +0900

    #2 #3 CSS 結合の順番保証と各種 Styus ファイルを追加

--- gulp/config.js
+++ gulp/config.js
@@ -1,3 +1,6 @@
+
+var stylus = './src/stylus/';
+
 /**
  * 汎用タスクから参照される設定です。
  *
@@ -5,16 +8,21 @@
  */
 module.exports = {
     /**
-     * タスク 'css' 用の設定です。Stylus ファイルを CSS にコンパイルします。
+     * タスク 'css' 用の設定です。Stylus ファイルを CSS にコンパイル & 結合します。
+     * 結合順を保証するため、src には明示的に対象ファイルを定義しています。
      * 以下の npm に依存しています。
      *
      * gulp gulp-load-plugins gulp-stylus gulp-concat gulp-plumber gulp-sourcemaps
-     *
-     * @type {Object}
      */
     css: {
-        // concat を考慮すると明示的に順番指定するほうが安全かも
-        src:    [ './src/stylus/*.styl' ],
+        src:    [ stylus + 'App.styl',
+                  stylus + 'Normalize.styl',
+                  stylus + 'Icon.styl',
+                  stylus + 'Content.styl',
+                  stylus + 'Header.styl',
+                  stylus + 'Footer.styl',
+                  stylus + 'Sidebar.styl'
+                ],
         dest :  './src',
         bundle: 'style.css'
     }
