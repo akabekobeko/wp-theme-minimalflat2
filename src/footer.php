bf2f6d9bdf76b88b681772b54369996c883bb7dd
akabeko <akabekobeko@gmail.com>
2015-07-05 16:57:10 +0900

    #32 サイトのフッター部分を BEM 化

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,4 @@
-  <footer class="footer">
+  <footer class="page__footer">
     Copyright &copy; 2009 - 2015 <?php  bloginfo( 'name' ); ?> All Rights Reserved.<br>
     Theme by <i class="minimalflat2-icon-github"></i> <a href="<?php echo esc_url( 'https://github.com/akabekobeko/wp-theme-minimalflat2', 'twentyfifteen' ); ?>" target="_blank">minimalflat2</a>, <i class="minimalflat2-icon-html5"></i> Valid HTML5 and <i class="minimalflat2-icon-css3"></i> CSS 3.
   </footer>
