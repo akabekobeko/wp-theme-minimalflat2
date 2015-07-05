09b7c7846a03e755307bcc8edd90ac1b3aaf869f
akabeko <akabekobeko@gmail.com>
2015-07-05 16:53:37 +0900

    #32 サイトのヘッダーを BEM 化

--- src/header.php
+++ src/header.php
@@ -8,11 +8,11 @@
 </head>
 <body <?php body_class(); ?>>
 <div class="page">
-  <header class="header">
-    <nav class="menu">
+  <header class="page__header">
+    <nav class="page__header__menu">
       <?php wp_nav_menu( array ( 'theme_location' => 'header-navi' ) ); ?>
       <div class="clarfix"></div>
     </nav>
-    <h1 class="title"><a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-    <h2 class="subtitle"><?php bloginfo( 'description' ); ?></h2>
+    <h1 class="page__header__title"><a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+    <h2 class="page__header__subtitle"><?php bloginfo( 'description' ); ?></h2>
   </header>
