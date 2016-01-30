47014fe877790858bcf0608a1abc5bc64c6766bb
akabeko <akabekobeko@gmail.com>
2016-01-30 17:02:07 +0900

    #41 Update a year

--- src/footer.php
+++ src/footer.php
@@ -1,5 +1,5 @@
   <footer class="page__footer">
-    Copyright &copy; 2009 - 2015 <?php  bloginfo( 'name' ); ?> All Rights Reserved.<br>
+    Copyright &copy; 2009 - 2016 <?php  bloginfo( 'name' ); ?> All Rights Reserved.<br>
     Theme by <i class="minimalflat2-icon-github"></i> <a href="<?php echo esc_url( 'https://github.com/akabekobeko/wp-theme-minimalflat2', 'twentyfifteen' ); ?>" target="_blank">minimalflat2</a>, <i class="minimalflat2-icon-html5"></i> Valid HTML5 and <i class="minimalflat2-icon-css3"></i> CSS 3.
   </footer>
 </div>
